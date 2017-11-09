da0dd534536a3bf2e7ef5779da633eebd7c4f597
Reltre <tglippert@gmail.com>
2017-11-08 20:13:07 -0800

    Remove rails rspec specific comments and commands from spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,28 +25,6 @@ ActiveRecord::Migration.maintain_test_schema!
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = false
-  config.include ControllerMacros, type: :controller
-  config.include FeatureMacros, type: :feature
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
