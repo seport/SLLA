77aeab72ccdb7beadb5e1f7638bbbd686d02154c
Reltre <tglippert@gmail.com>
2017-12-03 11:01:15 -0800

    Add pry gem to Gemfile.

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,5 +2,13 @@ class Admin::SessionsController < ApplicationController
   def new; end
 
   def create
+    binding.pry
+    @user = User.new()
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
