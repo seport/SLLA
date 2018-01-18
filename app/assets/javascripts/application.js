bf28851c3a74c680f4b9a10392dbb0ad4a3e39d0
Michael Mazzarella <michael_mazzarella@hotmail.com>
2018-01-17 19:59:50 -0800

    button closes now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,20 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).on('click','.closebubble',function(){
+	$('.bubble').remove();
+})
+
+$(document).on('click','.closealert',function(){
+	$('.alert').slideUp("slow");
+})
+
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
